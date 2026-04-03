c5da374a9db2e50462ffd19f2ee6edabaef6633a
sanjeev kumar <nk973711@gmail.com>
2026-04-03 14:28:41 +0530

    Fix product order migration dependencies

--- database/migrations/2022_03_13_072108_add_product_id_to_sales_table.php
+++ database/migrations/2022_03_13_072108_add_product_id_to_sales_table.php
@@ -14,19 +14,32 @@ class AddProductIdToSalesTable extends Migration
      */
     public function up()
     {
-        Schema::table('sales', function (Blueprint $table) {
-            $table->integer('product_order_id')->unsigned()->nullable()->after('promotion_id');
+        if (!Schema::hasColumn('sales', 'product_order_id')) {
+            Schema::table('sales', function (Blueprint $table) {
+                $table->integer('product_order_id')->unsigned()->nullable()->after('promotion_id');
+            });
+        }
 
+        Schema::table('sales', function (Blueprint $table) {
             DB::statement("ALTER TABLE `sales` MODIFY COLUMN `type` enum('webinar','meeting','subscribe','promotion','registration_package','product') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL AFTER `registration_package_id`");
         });
 
-        Schema::table('order_items', function (Blueprint $table) {
-            //$table->integer('product_id')->unsigned()->nullable()->after('registration_package_id');
-            $table->integer('product_order_id')->unsigned()->nullable()->after('product_id');
-        });
+        if (!Schema::hasColumn('order_items', 'product_id')) {
+            Schema::table('order_items', function (Blueprint $table) {
+                $table->integer('product_id')->unsigned()->nullable()->after('registration_package_id');
+            });
+        }
 
-        Schema::table('accounting', function (Blueprint $table) {
-            $table->integer('product_id')->unsigned()->nullable()->after('registration_package_id');
-        });
+        if (!Schema::hasColumn('order_items', 'product_order_id')) {
+            Schema::table('order_items', function (Blueprint $table) {
+                $table->integer('product_order_id')->unsigned()->nullable()->after('product_id');
+            });
+        }
+
+        if (!Schema::hasColumn('accounting', 'product_id')) {
+            Schema::table('accounting', function (Blueprint $table) {
+                $table->integer('product_id')->unsigned()->nullable()->after('registration_package_id');
+            });
+        }
     }
 }
