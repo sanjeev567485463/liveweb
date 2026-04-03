ed8ada77057ecd2bd8960eddd25caf819e38db7b
sanjeev kumar <nk973711@gmail.com>
2026-04-03 14:24:03 +0530

    Fix registration package sales migration order

--- database/migrations/2022_01_17_152605_add_registration_package_id_to_sales_table.php
+++ database/migrations/2022_01_17_152605_add_registration_package_id_to_sales_table.php
@@ -14,19 +14,29 @@ class AddRegistrationPackageIdToSalesTable extends Migration
      */
     public function up()
     {
-        Schema::table('sales', function (Blueprint $table) {
-            $table->integer('registration_package_id')->unsigned()->nullable()->after('promotion_id');
+        if (!Schema::hasColumn('sales', 'registration_package_id')) {
+            Schema::table('sales', function (Blueprint $table) {
+                $table->integer('registration_package_id')->unsigned()->nullable()->after('promotion_id');
+            });
+        }
 
+        Schema::table('sales', function (Blueprint $table) {
             DB::statement("ALTER TABLE `sales` MODIFY COLUMN `type` enum('webinar','meeting','subscribe','promotion','registration_package') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL AFTER `registration_package_id`");
         });
 
-        Schema::table('order_items', function (Blueprint $table) {
-            $table->integer('registration_package_id')->unsigned()->nullable()->after('promotion_id');
-        });
+        if (!Schema::hasColumn('order_items', 'registration_package_id')) {
+            Schema::table('order_items', function (Blueprint $table) {
+                $table->integer('registration_package_id')->unsigned()->nullable()->after('promotion_id');
+            });
+        }
+
+        if (!Schema::hasColumn('accounting', 'registration_package_id')) {
+            Schema::table('accounting', function (Blueprint $table) {
+                $table->integer('registration_package_id')->unsigned()->nullable()->after('promotion_id');
+            });
+        }
 
         Schema::table('accounting', function (Blueprint $table) {
-            $table->integer('registration_package_id')->unsigned()->nullable()->after('promotion_id');
-
             DB::statement("ALTER TABLE `accounting` MODIFY COLUMN `type_account` enum('income','asset','subscribe','promotion','registration_package') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL AFTER `type`");
         });
     }
