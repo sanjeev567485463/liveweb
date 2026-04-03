6f6a69f6434ca1773bd86b6d5fdabe8bb77108c5
sanjeev kumar <nk973711@gmail.com>
2026-04-03 15:26:32 +0530

    Add safe fallback for homepage rendering

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -4,27 +4,44 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $activeTheme = getActiveTheme();
-        $homeLanding = $activeTheme->homeLanding;
+        try {
+            $activeTheme = getActiveTheme();
 
-        $seoSettings = getSeoMetas('home');
-        $pageTitle = !empty($seoSettings['title']) ? $seoSettings['title'] : trans('home.home_title');
-        $pageDescription = !empty($seoSettings['description']) ? $seoSettings['description'] : trans('home.home_title');
-        $pageRobot = getPageRobot('home');
+            if (empty($activeTheme)) {
+                Log::warning('Homepage fallback triggered because no active theme was found.');
+                return redirect('/login');
+            }
 
-        $data = [
-            'pageTitle' => $pageTitle,
-            'pageDescription' => $pageDescription,
-            'pageRobot' => $pageRobot,
-            'activeTheme' => $activeTheme,
-            'homeLanding' => $homeLanding,
-        ];
+            $homeLanding = $activeTheme->homeLanding;
 
-        return view('design_1.web.home.index', $data);
+            $seoSettings = getSeoMetas('home');
+            $pageTitle = !empty($seoSettings['title']) ? $seoSettings['title'] : trans('home.home_title');
+            $pageDescription = !empty($seoSettings['description']) ? $seoSettings['description'] : trans('home.home_title');
+            $pageRobot = getPageRobot('home');
+
+            $data = [
+                'pageTitle' => $pageTitle,
+                'pageDescription' => $pageDescription,
+                'pageRobot' => $pageRobot,
+                'activeTheme' => $activeTheme,
+                'homeLanding' => $homeLanding,
+            ];
+
+            return response()->view('design_1.web.home.index', $data);
+        } catch (\Throwable $e) {
+            Log::error('Homepage render failed.', [
+                'message' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+            ]);
+
+            return redirect('/login');
+        }
     }
 }
